c1a8dbdfc169ac3749d0a83554e40ce8869f97c5
Maikol Rico perez <mrico@unitecnica.net>
2019-03-17 19:18:57 -0500

    cambios en los modelos

--- app/models/archive.rb
+++ app/models/archive.rb
@@ -1,5 +1,5 @@
 class Archive < ApplicationRecord
 	#has_and_belongs_to_many :categories
-	has_many :has_categories 
-	has_many :categories, through: :has_categories
+	#has_many :has_categories 
+	#has_many :categories, through: :has_categories
 end
