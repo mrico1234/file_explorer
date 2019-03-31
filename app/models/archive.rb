1ca5a994a1e90ab40061c2cdd1707f725489d849
Maikol Rico perez <mrico@unitecnica.net>
2019-03-30 21:57:28 -0500

    Se soluciono error de casilla q nuestra la ubicacion(ruta) de un archivo dentro del show de categories

--- app/models/archive.rb
+++ app/models/archive.rb
@@ -2,9 +2,9 @@ class Archive < ApplicationRecord
 	belongs_to :category
 
 	validates :name, presence:true, length: { minimum: 2}
-	validates :name, format: { with: /\A[0-9a-zA-Z]+\z/, message: "Solo se permiten letras y numeros" }
+	#validates :name, format: { with: /\A[0-9a-zA-Z]+\z/, message: "Solo se permiten letras y numeros" }
 	validates :typ, presence:true
 	validates :tamaño, presence:true, length: { minimum: 3}
-	validates :tamaño, format: { with: /\A[0-9a-zA-Z]+\z/, message: "Solo se permiten letras y numeros" }
+	#validates :tamaño, format: { with: /\A[0-9a-zA-Z]+\z/, message: "Solo se permiten letras y numeros" }
 
 end
--- app/models/category.rb
+++ app/models/category.rb
@@ -3,6 +3,6 @@ class Category < ApplicationRecord
 	has_many :archives
 
 	validates :name, presence:true, length: { minimum: 2}
-	validates :name, format: { with: /\A[0-9a-zA-Z]+\z/, message: "Solo se permiten letras y numeros" }
+	#validates :name, format: { with: /\A[0-9a-zA-Z]+\z/, message: "Solo se permiten letras y numeros" }
   #validates :bio, length: { maximum: 500 }
 end
