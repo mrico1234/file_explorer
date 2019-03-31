18177ec6b4978b51061c94fcecc2bfd8f15eba6a
Maikol Rico perez <mrico@unitecnica.net>
2019-03-30 21:15:32 -0500

    Se agregaron validaciones a los formularios de los modelos

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,4 +1,8 @@
 class Category < ApplicationRecord
 	has_ancestry #relación con gema ancestry
 	has_many :archives
+
+	validates :name, presence:true, length: { minimum: 2}
+	validates :name, format: { with: /\A[0-9a-zA-Z]+\z/, message: "Solo se permiten letras y numeros" }
+  #validates :bio, length: { maximum: 500 }
 end
